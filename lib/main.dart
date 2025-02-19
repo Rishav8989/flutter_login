ffc19e74624963cd1016e5568f31831150845e51
rishav8989 <betusenger@gmail.com>
2025-02-20 01:38:56 +0530

    seprate authcheck

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:login/utils/app_theme.dart';
-import 'login_page.dart';
-import 'home_page.dart'; // Import HomePage
+import 'package:login/utils/auth_check.dart';
 import 'package:pocketbase/pocketbase.dart';
-import 'package:shared_preferences/shared_preferences.dart'; // Import shared_preferences
 
 final pb = PocketBase('https://first.pockethost.io/');
 
@@ -18,68 +16,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Notes App',
-      theme: darkTheme,       // Use appTheme from app_theme.dart
-      // Remove darkTheme and themeMode as you are using only default theme
+      theme: amoled, // Directly using lightTheme
+      themeMode: ThemeMode.system, // Keep or remove themeMode based on if you want system theme or always light
       debugShowCheckedModeBanner: false,
       home: const AuthCheck(),
-    );
-  }
-}
-
-class AuthCheck extends StatefulWidget {
-  const AuthCheck({super.key});
-
-  @override
-  State<AuthCheck> createState() => _AuthCheckState();
-}
-
-class _AuthCheckState extends State<AuthCheck> {
-  Future<String?> _getCachedToken() async {
-    final prefs = await SharedPreferences.getInstance();
-    return prefs.getString('pb_auth_token');
-  }
-
-  Future<void> _loadCachedToken() async {
-    final cachedToken = await _getCachedToken();
-    if (cachedToken != null) {
-      pb.authStore.save(cachedToken, pb.authStore.record);
-      print('Cached token loaded.');
-    } else {
-      print('No cached token found.');
-    }
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _checkAuth();
-  }
-
-  Future<void> _checkAuth() async {
-    await _loadCachedToken();
-    await Future.delayed(Duration.zero);
-
-    if (pb.authStore.isValid) {
-      print('User is already logged in (Token: ${pb.authStore.token})');
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => const HomePage()),
-      );
-    } else {
-      print('User is not logged in.');
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => const LoginPage()),
-      );
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: CircularProgressIndicator(),
-      ),
     );
   }
 }
---
+++ lib/utils/auth_check.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:login/home_page.dart';
+import 'package:login/login_page.dart';
+import 'package:login/main.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+
+class AuthCheck extends StatefulWidget {
+  const AuthCheck({super.key});
+
+  @override
+  State<AuthCheck> createState() => _AuthCheckState();
+}
+
+class _AuthCheckState extends State<AuthCheck> {
+  Future<String?> _getCachedToken() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString('pb_auth_token');
+  }
+
+  Future<void> _loadCachedToken() async {
+    final cachedToken = await _getCachedToken();
+    if (cachedToken != null) {
+      pb.authStore.save(cachedToken, pb.authStore.record);
+      print('Cached token loaded.');
+    } else {
+      print('No cached token found.');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _checkAuth();
+  }
+
+  Future<void> _checkAuth() async {
+    await _loadCachedToken();
+    await Future.delayed(Duration.zero);
+
+    if (pb.authStore.isValid) {
+      print('User is already logged in (Token: ${pb.authStore.token})');
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const HomePage()), // Navigate to HomePage
+      );
+    } else {
+      print('User is not logged in.');
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const LoginPage()), // Navigate to LoginPage
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
